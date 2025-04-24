694f3da0846fc7ec952775bf9ccf8c83e8173769
yesserlab <yesser@recogni.com>
2025-04-24 11:56:36 +0200

    Patch for our usecase

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,2 @@
 `dirname "$0"`/download.sh
-
-`dirname "$0"`/build.sh lua-5.0.3
-`dirname "$0"`/build.sh lua-5.1.5
-`dirname "$0"`/build.sh lua-5.2.4
-`dirname "$0"`/build.sh lua-5.3.6
-`dirname "$0"`/build.sh lua-5.4.2
+`dirname "$0"`/build.sh lua-5.4.6
