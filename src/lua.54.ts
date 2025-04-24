96064f5fb63e1548d67142ad92a67fdd9f9a612c
yesserlab <yesser@recogni.com>
2025-04-24 12:00:13 +0200

    Deleted useless old versions

--- src/lua.54.ts
+++ src/lua.54.ts
@@ -1,11 +1,11 @@
 import { createLauxLib, createLua, createLuaLib } from "./binding-factory";
-import glue from "./glue/glue-lua-5.4.2";
+import glue from "./glue/glue-lua-5.4.6";
 
 let luaGlue = glue({
     print: console.log,
     printErr: console.error,
 });
 
-export const lua = createLua(luaGlue, "5.4.2");
-export const lauxlib = createLauxLib(luaGlue, lua, "5.4.2");
-export const lualib = createLuaLib(luaGlue, "5.4.2");
+export const lua = createLua(luaGlue, "5.4.6");
+export const lauxlib = createLauxLib(luaGlue, lua, "5.4.6");
+export const lualib = createLuaLib(luaGlue, "5.4.6");
